78c66fa9a7b258d42d700d835bd8b6168c21746f
Isaac <mugwimiisaac230@gmail.com>
2026-02-17 19:30:21 +0300

    stopped all the console messages

--- frontend/brightcoders-frontend/vite.config.js
+++ frontend/brightcoders-frontend/vite.config.js
@@ -2,7 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // base:process.env.NODE_ENV === 'production' ? '/brightcoders-frontend/' : '/',
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    esbuild: {
+      // mode is 'production' when you run 'vite build' (which Vercel does)
+      drop: mode === 'production' ? ['console', 'debugger'] : [],
+    },
+  }
 })
